9e6635cca17fb5c686ba425f38b42c9c02bd3a5f
root <root@DESKTOP-BJMH8SP>
2024-05-23 23:49:38 +0700

    subspyder v1

--- install_go.sh
+++ install_go.sh
@@ -3,17 +3,18 @@
 wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
 
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-echo 'export GOPATH=$HOME/go' >> ~/.profile
-echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.profile
-source ~/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
+source ~/.bash_profile
 
 rm go1.22.3.linux-amd64.tar.gz
 
-go version
+if command -v go &> /dev/null
+then
+    go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-sudo ln -s ~/go/bin/subfinder /usr/local/bin/subfinder
+    sudo ln -s ~/go/bin/subfinder /usr/local/bin/subfinder
 
-
+    echo -e "\e[32mGo and Subfinder installation success!\e[0m"
+else
+    echo -e "\e[31mFailed to install Go. Please check your internet connection and try again.\e[0m"
+fi
